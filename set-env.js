462b07a437e972b8ae965267bbe113774ffd2d15
Barış Ertuğrul <b.ertugrul.eposta@gmail.com>
2021-12-10 02:14:36 +0300

    Code Refactoring for Deploy

--- set-env.js
+++ set-env.js
@@ -5,12 +5,18 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 const targetPath = isProduction
-  ? './src/environments/environment.prod.ts'
-  : './src/environments/environment.ts';
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
 
-const envFileContent = 'export const environment = {production:"${isProduction}", BASE_URL: "${process.env.BASE_URL}"};';
-writeFile(targetPath,envFileContent, (err) =>{
-  if(err){
+const envFileContent = `
+export const environment = {
+  production: ${isProduction},
+  BASE_URL: "${process.env.BASE_URL}"
+};
+`;
+
+writeFile(targetPath, envFileContent, (err) => {
+  if (err) {
     throw console.error(err);
   }
 })
