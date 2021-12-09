fca93c1ff5dd40d4627107c18848ec219756ee8a
Barış Ertuğrul <b.ertugrul.eposta@gmail.com>
2021-12-10 01:47:12 +0300

    Deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
-//export const environment = {production:`${isProduction}`, BASE_URL: `${process.env.BASE_URL}`};
-export const environment = {
-   production: false,
-   BASE_URL: 'http://localhost:8080'
-};
+export const environment = {production:`${isProduction}`, BASE_URL: `${process.env.BASE_URL}`};
+// export const environment = {
+//    production: false,
+//    BASE_URL: 'http://localhost:8080'
+// };
